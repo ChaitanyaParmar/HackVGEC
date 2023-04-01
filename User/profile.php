8f138bc76e2cc582f0146b3b05eda89b181314e9
Chaitanya <68532604+ChaitanyaParmar@users.noreply.github.com>
2023-04-01 09:55:38 +0530

    1 April

---
+++ User/profile.php
@@ -0,0 +1,58 @@
+<?php include 'controller.php' ?>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en-US" lang="en-US">
+<head>
+    <!-- Basic Page Needs -->
+    <meta charset="utf-8">
+    <!--[if IE]><meta http-equiv='X-UA-Compatible' content='IE=edge,chrome=1'><![endif]-->
+    <title>VGEC | Vishwakarma Government Engineering College</title>
+
+    <meta name="author" content="themesflat.com">
+
+    <!-- Mobile Specific Metas -->
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+
+    <!-- Theme Style -->
+    <link rel="stylesheet" type="text/css" href="assets/dist/style.css">
+
+    <!-- Reponsive -->
+    <link rel="stylesheet" type="text/css" href="assets/dist/responsive.css">
+
+     <!-- Favicon and Touch Icons  -->
+     <link rel="shortcut icon" href="assets/images/home/vgec1.jpg">
+    <link rel="apple-touch-icon-precomposed" href="assets/home/vgec1.jpg">
+
+</head>
+<body class="body header-fixed">
+
+    <!-- preloade -->
+    <div class="preload preload-container">
+        <div class="preload-logo"></div>
+    </div>
+<?php 
+            include 'navbar.php' ?>                
+           
+<?php include 'footer1.php' ?>
+        <?php include 'footer.php' ?>
+    <a id="scroll-top"></a>
+
+    <!-- Javascript -->
+
+    <script src="assets/js/jquery.min.js"></script>
+    <script src="assets/js/jquery.easing.js"></script>
+    <script src="assets/js/bootstrap.min.js"></script>
+    <script src="assets/js/swiper-bundle.min.js"></script>
+    <script src="assets/js/swiper.js"></script>
+
+    <script src="assets/js/plugin.js"></script>
+    <script src="assets/js/count-down.js"></script>
+    <script src="assets/js/shortcodes.js"></script>
+    <script src="assets/js/main.js"></script>
+
+    <script src="assets/js/wow.min.js"></script>
+    <script src="assets/js/switchmode.js"></script>
+
+</body>
+
+</html>
